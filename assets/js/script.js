cc3c82b8e36c80a048150dab0ad765f97eefa6a2
Cameron Harvey <charvey0@gmail.com>
2021-01-31 18:51:24 -0800

    Set up information display for search results

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,16 @@ var display1 = $("#display1").append("Display 1");
 var display2 = $("#display2").append("Display 2");
 var display3 = $("#display3").append("Display 3");
 var cities = $("#cities");
+var current = $("#current");
+
+var lat = "47.6062";
+var lon = "-122.3321";
 
 /*************************************************
                     VARIABLES AND CONSTANTS
 **************************************************/
-var urlBase = "http://api.openweathermap.org/";
-var urlEnd = "&appid=5af7e440df7c59ead813eff5795179d2";
+var urlBase = "https://api.openweathermap.org/";
+var urlEnd = "&appid=5af7e440df7c59ead813eff5795179d2&units=imperial";
 
 
 /*************************************************
@@ -27,7 +31,19 @@ function firstSearch(){
     return true;    
 }
     
-    
+// looks up the UV index then returns the div to be displayed
+function getUV(lat, lon) {
+    var html = '<p>UV Index: ';
+    var request = urlBase + "data/2.5/uvi?lat=" + lat + "&lon=" + lon + urlEnd;
+    fetch (request)
+    .then(response => response.json())
+    .then(function (data) {
+        html += data.value;
+        html += '</p>';
+        current.append(html);
+    });
+}
+        
 
 /*************************************************
                     USER INTERACTION FUNCTIONS
@@ -51,7 +67,7 @@ function init() {
   if (firstSearch()) { 
     display1.hide();   
   } else { 
-    display2.hide(); 
+    display2.hide();
   }
 
 // */
@@ -65,7 +81,15 @@ $("#searchBtn").on("click", function() {
     var request = urlBase + "data/2.5/weather?q=" + searchCity + urlEnd;
     fetch(request) 
     .then(response => response.json())
-    .then(data => console.log(data));
-    
+    .then(function (data) {
+       lat = data.coord.lat;
+       lon = data.coord.lon;
+       var html = '<h4 class="card-title">Current conditions in '+data.name+':</h4>';
+       html += "<p>Temperature: "+data.main.temp+" &deg;F</p>";
+       html += "<p>Humidity: "+data.main.humidity+"%</p>";
+       html += "<p>Wind Speed: "+data.wind.speed+" MPH</p>";
+       current.append(html);
+    });
+    getUV(lat, lon);
 });
 
